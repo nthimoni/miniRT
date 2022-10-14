9e5722359c9bc6b08149f55214ad8792241b7829
Romain MOREL <rmorel@e3r10p16.clusters.42paris.fr>
2022-10-14 18:26:33 +0200

    Intersection des matrices OK a premiere vue. Plus de tests a faire, et rajouter d'autre spheres.

--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:13:15 by rmorel            #+#    #+#             */
-/*   Updated: 2022/10/14 15:42:45 by rmorel           ###   ########.fr       */
+/*   Updated: 2022/10/14 18:24:21 by rmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,16 +42,17 @@ void	init_inter(t_rt *rt)
 
 void	init_pixel(t_rt *rt)
 {
+	t_intersect	inter;
 	int			i;
 	int			j;
-	t_intersect	inter;
 
 	i = 0;
 	j = 0;
-	while (i < 0)
+	while (i < W_W)
 	{
-		while (j++ < 1)
+		while (j++ < W_H)
 		{
+			printf("[%d][%d]\n", i, j);
 			ft_bzero(&inter, sizeof(t_intersect));
 			pixel_raster_to_space(&inter, i, j, rt);
 			intersect_obj(rt, &inter, i, j);
@@ -59,25 +60,22 @@ void	init_pixel(t_rt *rt)
 		j = 0;
 		i++;
 	}
+	pixel_raster_to_space(&inter, 500, 250, rt);
+	pixel_raster_to_space(&inter, 1500, 250, rt);
+	pixel_raster_to_space(&inter, 500, 750, rt);
+	pixel_raster_to_space(&inter, 1500, 750, rt);
 }
 
 void	pixel_raster_to_space(t_intersect *i, int x, int y, t_rt *rt)
 {
 	i->pixel.x = (x + 0.5) / W_W;
 	i->pixel.y = (y + 0.5) / W_H;
-	if (i->pixel.x > 0.5)
-		i->pixel.x = 2 * i->pixel.x - 1;
-	else
-		i->pixel.x = 1 - 2 * i->pixel.x;
-	if (i->pixel.y > 0.5)
-		i->pixel.y = 2 * i->pixel.y - 1;
-	else
-		i->pixel.y = 1 - 2 * i->pixel.y;
-	i->pixel.x = (2 * i->pixel.x - 1) * W_W / W_H * tan(rt->scn.cam.FOV / 2);	
-	i->pixel.y = (2 * i->pixel.y - 1) * tan(rt->scn.cam.FOV / 2);	
-	i->pixel.z = 1;
+	i->pixel.x = 2 * i->pixel.x - 1;
+	i->pixel.y = 1 - 2 * i->pixel.y;
+	i->pixel.x = i->pixel.x * W_W / W_H * tan(rt->scn.cam.FOV / 2);	
+	i->pixel.y = i->pixel.y * tan(rt->scn.cam.FOV / 2);	
+	i->pixel.z = -1;
 	i->pixel.w = 1;
-	printf("angle = %lf\n", rt->scn.cam.FOV);
 	printf("i->pixel.x = %lf, y = %lf\n", i->pixel.x, i->pixel.y);
 	ft_bzero(&i->ray.o, sizeof(t_tuple));
 	i->ray.o.w = 1;
@@ -87,7 +85,7 @@ void	pixel_raster_to_space(t_intersect *i, int x, int y, t_rt *rt)
 	i->ray.d.w = 0;
 	mult_tuple_matrix_4(&i->ray.o, rt->ctow_m, i->ray.o);
 	mult_tuple_matrix_4(&i->ray.d, rt->ctow_m, i->ray.d);
-	printf("i->ray.d.x = %lf, y = %lf\n", i->ray.d.x, i->ray.d.y);
+	printf("i->ray.d.x = %lf, y = %lf, z = %lf\n", i->ray.d.x, i->ray.d.y, i->ray.d.z);
 	norm_v3(&i->ray.d);
 }
 
@@ -95,19 +93,19 @@ void	test_sphere_init(t_rt *rt)
 {
 	ft_bzero(&rt->scn.cam, sizeof(t_obj));
 	ft_bzero(&rt->scn.sph, sizeof(t_obj));
-	rt->scn.cam.o.x = 1;
-	rt->scn.cam.o.y = 1;
-	rt->scn.cam.o.z = 1;
+	rt->scn.cam.o.x = 0;
+	rt->scn.cam.o.y = 0;
+	rt->scn.cam.o.z = 0;
 	rt->scn.cam.o.w = 1;
 	rt->scn.cam.d.x = 0;
 	rt->scn.cam.d.y = 0;
 	rt->scn.cam.d.z = 1;
 	rt->scn.cam.d.w = 0;
 	norm_v3(&rt->scn.cam.d);
-	rt->scn.sph.o.x = 0;
-	rt->scn.sph.o.y = 0;
-	rt->scn.sph.o.z = 20;
-	rt->scn.sph.o.w = 10;
+	rt->scn.sph.o.x = 30;
+	rt->scn.sph.o.y = 40;
+	rt->scn.sph.o.z = 200;
+	rt->scn.sph.o.w = 1;
 	rt->scn.sph.diam = 20;
 	rt->scn.cam.FOV = M_PI * 70 / 180;	
 	rt->scn.sph.color = create_trgb(0, 255, 0, 0);
@@ -154,17 +152,21 @@ void	intersect_obj(t_rt *rt, t_intersect *inter, int i, int j)
 	t_quadra	q;
 	t_tuple		s_to_r;
 
+	//print_tuple(&rt->scn.sph.o, "sph.o");
+	//print_tuple(&inter->ray.o, "ray.o");
 	s_to_r = create_v3(rt->scn.sph.o, inter->ray.o);
-	print_tuple(&inter->ray.d, "inter->ray.d");
+	//print_tuple(&s_to_r, "s_to_r");
 	q.a = dot_product_v3(inter->ray.d, inter->ray.d);
-	q.b = dot_product_v3(inter->ray.d, s_to_r);
+	q.b = 2 * dot_product_v3(inter->ray.d, s_to_r);
 	q.c = dot_product_v3(s_to_r, s_to_r) - pow(rt->scn.sph.diam / 2, 2); 
-	printf("q.a = %lf q.b = %lf q.c = %lf\n", q.a, q.b, q.c);
+	//printf("dot_product(s_to_r) = %lf pow(rt->scn.sph.diam / 2, 2) = %lf\n", dot_product_v3(s_to_r, s_to_r), pow(rt->scn.sph.diam / 2, 2)); 
+	//printf("q.a = %lf q.b = %lf q.c = %lf\n", q.a, q.b, q.c);
 	if (!solve_quadratic(inter, q))
 		return ;
 	else
 		inter->obj = &rt->scn.sph; 
-	my_mlx_pixel_put(rt, i, j, rt->scn.sph.color);	
+	my_mlx_pixel_put(rt, i, j, rt->scn.sph.color);
+	printf("true\n");
 }
 
 void	get_matrix_align_v1_v2(t_u m[4][4], t_tuple v1, t_tuple v2)
@@ -174,7 +176,6 @@ void	get_matrix_align_v1_v2(t_u m[4][4], t_tuple v1, t_tuple v2)
 	t_u		k;
 
 	u = cross_product_v3(v1, v2);
-	print_tuple(&u, "u");
 	cos_a = dot_product_v3(v1, v2);
 	k = 1.0 / (1.0 + cos_a);
 	m[0][0] = u.x * u.x * k + cos_a;
@@ -207,7 +208,7 @@ void	world_to_camera(t_rt *rt)
 	cam_space.w = 0;
 	norm_v3(&cam_space);
 	if (check_vector_opposite(cam_space, rt->scn.cam.d))
-		scale_matrix_4(tmp, -1, -1, -1);
+		rot_y_matrix_4(tmp, M_PI);
 	else
 		get_matrix_align_v1_v2(tmp, rt->scn.cam.d, cam_space);
 	trans_matrix_4(trans_m4, -rt->scn.cam.o.x, -rt->scn.cam.o.y, -rt->scn.cam.o.z);
