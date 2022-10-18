2d3e0a3246fda84e08531ff6bd47d3f0f6b7ef89
Nicolas THIMONIER <nthimoni@e2r6p10.clusters.42paris.fr>
2022-10-18 23:43:06 +0200

    debut lighting

--- include/lighting.h
+++ include/lighting.h
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   color.h                                            :+:      :+:    :+:   */
+/*   lighting.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/14 13:07:29 by rmorel            #+#    #+#             */
-/*   Updated: 2022/10/18 23:11:25 by nthimoni         ###   ########.fr       */
+/*   Created: 2022/10/18 21:46:19 by nthimoni          #+#    #+#             */
+/*   Updated: 2022/10/18 21:47:36 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COLOR_H
-#define COLOR_H 
+#ifndef LIGHTING_H
+# define LIGHTING_H 
 
-int	create_trgb(int t, int r, int g, int b);
-int	get_t(int trgb);
-int	get_r(int trgb);
-int	get_g(int trgb);
-int	get_b(int trgb);
-int	scale_color(int color, double coef);
+#include "data_struct.h"
+
+int	lighting(t_rt *rt, t_intersect *inter);
 
 #endif 
--- src/colors.c
+++ src/colors.c
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 19:20:19 by rmorel            #+#    #+#             */
-/*   Updated: 2022/10/14 13:07:24 by rmorel           ###   ########.fr       */
+/*   Updated: 2022/10/18 23:10:45 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,18 @@ int	get_g(int trgb)
 int	get_b(int trgb)
 {
 	return (trgb & 0xFF);
+}
+
+int	scale_color(int color, double coef)
+{
+	int	t;
+	int	r;
+	int	g;
+	int	b;
+
+	t = get_t(color) * coef;
+	r = get_r(color) * coef;
+	g = get_g(color) * coef;
+	b = get_b(color) * coef;
+	return (create_trgb(t, r, g, b));
 }
