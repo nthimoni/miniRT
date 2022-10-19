50067778b611fcf7f68615bb1cd848ba7ea2869c
Nicolas THIMONIER <nthimoni@e1r6p6.clusters.42paris.fr>
2022-10-19 17:33:03 +0200

    incorrect normal vecotr for lighting sphere

--- src/lighting.c
+++ src/lighting.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 21:47:50 by nthimoni          #+#    #+#             */
-/*   Updated: 2022/10/19 01:41:24 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/10/19 15:20:05 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,18 @@ static int	set_specular(t_rt *rt, t_intersect *inter)
 	t_tuple	normal;
 	t_u		cos_n_l;
 
+	//mult_tuple_matrix_4(&inter->pixel, rt->ctow_m, inter->pixel);
 	light = sub_tupple(((t_obj *)rt->scn.light->content)->o, inter->pixel);
 	norm_v3(&light);
-	//mult_tuple_matrix_4(&inter->pixel, rt->ctow_m, inter->pixel);
+	mult_tuple_matrix_4(&inter->pixel, rt->ctow_m, inter->pixel);
 	normal = sub_tupple(inter->pixel, inter->obj->o);
+	//printf("length : %lf\n", length_v3(normal));
 	norm_v3(&normal);
-	print_tuple(&((t_obj *)rt->scn.light->content)->o, "position");
+	print_tuple(&inter->pixel, "inter");
 	cos_n_l = dot_product_v3(light, normal);
 	printf("cos : %lf\n", cos_n_l);
 	print_tuple(&normal, "normal");
-	if (cos_n_l > 0)
+	if (cos_n_l < 0)
 		return (create_trgb(0, 0, 0, 0));
 	return (scale_color(inter->obj->color, cos_n_l));
 }
