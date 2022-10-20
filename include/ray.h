fb6bff60b7a889c2a481b07d474b72368fbb8bf5
Romain MOREL <rmorel@e2r10p10.clusters.42paris.fr>
2022-10-20 15:21:36 +0200

    Trying to calcult sphere intersect in object space

--- include/ray.h
+++ include/ray.h
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:13:31 by rmorel            #+#    #+#             */
-/*   Updated: 2022/10/18 18:25:28 by rmorel           ###   ########.fr       */
+/*   Updated: 2022/10/20 15:16:10 by rmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,10 @@ void	init_pixel(t_rt *rt);
 void	pixel_raster_to_space(t_intersect *i, int x, int y, t_rt *rt);
 t_bool	solve_quadratic(t_intersect *inter, t_quadra q);
 int		ft_sign(t_u i);
+void	fill_obj(t_rt *rt);
 void	intersect_obj(t_rt *rt, t_intersect *inter, int i, int j);
 void	intersect_sph(t_obj	*sph, t_intersect *inter);
+void	intersect_sph2(t_obj *sph, t_intersect *inter);
 void	intersect_plane(t_obj *plane, t_intersect *inter);
 void	get_matrix_align_v1_v2(t_u m[4][4], t_tuple v1, t_tuple v2);
 t_bool	check_vector_opposite(t_tuple v1, t_tuple v2);
