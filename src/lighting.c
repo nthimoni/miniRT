f19b118e11815e977d259ce6b9c7d80fc1b57283
Nicolas THIMONIER <nthimoni@e1r6p15.clusters.42paris.fr>
2022-10-24 20:04:31 +0200

    synthese soustractive

--- src/lighting.c
+++ src/lighting.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 21:47:50 by nthimoni          #+#    #+#             */
-/*   Updated: 2022/10/20 17:53:29 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/10/24 15:36:27 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,26 @@ static int	set_specular(t_rt *rt, t_intersect *inter)
 	normal = sub_tupple(position, inter->obj->o);
 	norm_v3(&normal);
 	cos_n_l = dot_product_v3(light, normal);
-	if (cos_n_l > 0)
-		return (create_trgb(0, 90, 90, 0));
-	return (scale_color(inter->obj->color, cos_n_l));
+	if (cos_n_l >= -0.01)
+		return (scale_color(inter->obj->color, 0));
+	return (scale_color(inter->obj->color, 1.0 - cos_n_l));
 }
 
 int	lighting(t_rt *rt, t_intersect *inter)
 {
 	int	ambiant;
 	int	specular;
+	int	final;
 
 	ambiant = scale_color(rt->scn.amb.color, rt->scn.amb.ratio);
 	specular = set_specular(rt, inter);
+	specular = scale_color(specular, ((t_obj *)rt->scn.light->content)->ratio);
 	(void)ambiant, (void)specular;
+	final = create_trgb(0, get_r(ambiant) + get_r(specular),
+			get_g(ambiant) + get_g(specular), get_b(ambiant) + get_b(specular));
 	//return (inter->obj->color);
+	final = scale_color(final, 0.8);
+	return final;
 	if (specular == 0)
 		return (ambiant);
 	return (specular);
