97f45640f1e294731e3edaa139eed13ecc26e830
Nicolas THIMONIER <nthimoni@e1r10p1.clusters.42paris.fr>
2022-10-27 22:06:44 +0200

    specular highlight - ok / multispot - not ok

--- src/colors.c
+++ src/colors.c
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 19:20:19 by rmorel            #+#    #+#             */
-/*   Updated: 2022/10/18 23:10:45 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/10/26 19:30:04 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,21 @@ int	scale_color(int color, double coef)
 	int	g;
 	int	b;
 
-	t = get_t(color) * coef;
-	r = get_r(color) * coef;
-	g = get_g(color) * coef;
-	b = get_b(color) * coef;
+	if (get_t(color) * coef > 255.0)
+		t = 255;
+	else
+		t = get_t(color) * coef;
+	if (get_r(color) * coef > 255.0)
+		r = 255;
+	else
+		r = get_r(color) * coef;
+	if (get_g(color) * coef > 255.0)
+		g = 255;
+	else
+		g = get_g(color) * coef;
+	if (get_b(color) * coef > 255.0)
+		b = 255;
+	else
+		b = get_b(color) * coef;
 	return (create_trgb(t, r, g, b));
 }
--- src/lighting.c
+++ src/lighting.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 21:47:50 by nthimoni          #+#    #+#             */
-/*   Updated: 2022/10/24 15:36:27 by nthimoni         ###   ########.fr       */
+/*   Updated: 2022/10/27 22:05:29 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,30 @@
 #include "vector.h"
 #include "color.h"
 
-static t_tuple find_pos_inter(t_intersect *inter, t_rt *rt)
+int	set_specular(t_tuple *normal, t_tuple *light, t_tuple *position, t_rt *rt)
+{
+	t_tuple	cam;
+	t_tuple ref;
+	t_u		cos_r_c;
+	t_u		factor;
+	int		color;
+
+	ref.x = normal->x + (normal->x - light->x);
+	ref.y = normal->y + (normal->y - light->y);
+	ref.z = normal->z + (normal->z - light->z);
+	cam = sub_tupple(rt->scn.cam.o, *position);
+	norm_v3(&cam);
+	norm_v3(&ref);
+	cos_r_c= dot_product_v3(ref, cam);
+	if (cos_r_c >= -0.01)
+		return (0);
+	factor = pow(cos_r_c, 350);
+	color = scale_color(((t_obj *)rt->scn.light->content)->color, factor);
+	color = scale_color(color, ((t_obj *)rt->scn.light->content)->ratio);
+	return (color);
+}
+
+static t_tuple	find_pos_inter(t_intersect *inter, t_rt *rt)
 {
 	t_tuple	tmp;
 
@@ -24,7 +47,7 @@ static t_tuple find_pos_inter(t_intersect *inter, t_rt *rt)
 	return (add_tupple(rt->scn.cam.o, tmp));
 }
 
-static int	set_specular(t_rt *rt, t_intersect *inter)
+static int	set_diffuse(t_rt *rt, t_intersect *inter, int *specular)
 {
 	t_tuple	light;
 	t_tuple	normal;
@@ -37,27 +60,65 @@ static int	set_specular(t_rt *rt, t_intersect *inter)
 	normal = sub_tupple(position, inter->obj->o);
 	norm_v3(&normal);
 	cos_n_l = dot_product_v3(light, normal);
-	if (cos_n_l >= -0.01)
-		return (scale_color(inter->obj->color, 0));
-	return (scale_color(inter->obj->color, 1.0 - cos_n_l));
+	*specular = set_specular(&normal, &light, &position, rt);
+	if (cos_n_l <= 0.01)
+	{
+		//*specular = 0;
+		return (scale_color(((t_obj *)rt->scn.light->content)->color, 0));
+	}
+	return (scale_color(((t_obj *)rt->scn.light->content)->color, cos_n_l));
+}
+
+static int	sub_synthese(int obj_color, int light_color, double ratio_light)
+{
+	int		r;
+	int		g;
+	int		b;
+	double	coef;
+
+	(void)ratio_light;
+	coef = (get_r(light_color) * 1.0) / 255.0;
+	r = get_r(obj_color) * coef * ratio_light;
+	coef = (get_g(light_color) * 1.0) / 255.0;
+	g = get_g(obj_color) * coef * ratio_light;
+	coef = (get_b(light_color) * 1.0) / 255.0;
+	b = get_b(obj_color) * coef * ratio_light;
+	return (create_trgb(0, r, g, b));
+}
+
+static void	inc_color(int *color, int r, int g, int b)
+{
+	if (r + get_r(*color) > 255)
+		r = 255;
+	else
+		r += get_r(*color);
+	if (g + get_g(*color) > 255)
+		g = 255;
+	else
+		g += get_g(*color);
+	if (b + get_b(*color) > 255)
+		b = 255;
+	else
+		b += get_b(*color);
+	*color = create_trgb(0, r, g, b);
 }
 
 int	lighting(t_rt *rt, t_intersect *inter)
 {
 	int	ambiant;
+	int	diffuse;
 	int	specular;
 	int	final;
 
-	ambiant = scale_color(rt->scn.amb.color, rt->scn.amb.ratio);
-	specular = set_specular(rt, inter);
-	specular = scale_color(specular, ((t_obj *)rt->scn.light->content)->ratio);
-	(void)ambiant, (void)specular;
-	final = create_trgb(0, get_r(ambiant) + get_r(specular),
-			get_g(ambiant) + get_g(specular), get_b(ambiant) + get_b(specular));
-	//return (inter->obj->color);
-	final = scale_color(final, 0.8);
+	ambiant = sub_synthese(inter->obj->color, rt->scn.amb.color, rt->scn.amb.ratio);
+	diffuse = set_diffuse(rt, inter, &specular);
+	(void)ambiant, (void)diffuse;
+	diffuse = sub_synthese(inter->obj->color, diffuse, ((t_obj *)rt->scn.light->content)->ratio);
+	final = create_trgb(0, get_r(ambiant), get_g(ambiant), get_b(ambiant));
+	inc_color(&final, get_r(diffuse), get_g(diffuse), get_b(diffuse));
+	inc_color(&final, get_r(specular), get_g(specular), get_b(specular));
 	return final;
-	if (specular == 0)
+	if (diffuse == 0)
 		return (ambiant);
-	return (specular);
+	return (diffuse);
 }
