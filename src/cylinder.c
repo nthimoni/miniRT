c89ca0659232ec14b109487d62f70b34f25abe95
rmorel <rmorel@student.42.fr>
2022-12-30 13:00:26 +0100

    Cylinder seems ok, not normined

--- src/cylinder.c
+++ src/cylinder.c
@@ -6,16 +6,19 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 12:15:54 by rmorel            #+#    #+#             */
-/*   Updated: 2022/12/28 21:36:43 by rmorel           ###   ########.fr       */
+/*   Updated: 2022/12/30 12:34:39 by rmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray.h"
 
-static 		t_tuple	find_pos_inter(t_ray ray, t_u t0);
+static 	t_tuple	find_pos_inter(t_ray ray, t_u t0);
 void	check_cylinder(t_obj *cyl, t_ray *ray2, t_intersect *inter);
-void	add_inter(t_intersect *inter, t_ray ray, int index, t_obj *cyl);
-void 	endcap(t_intersect *inter, t_obj *cyl, t_tuple cyl_obj[2], t_ray *ray2);
+void 	intersect_endcap(t_intersect *inter, t_obj *cyl, t_ray *ray2);
+void	normal_endcap(t_tuple point, t_obj *cyl, t_tuple *normal);
+void	normal_cylinder(t_tuple point, t_obj *cyl, t_tuple *normal);
+void	add_inter0(t_intersect *i, t_obj *o, t_u t);
+void	add_inter1(t_intersect *i, t_obj *o, t_u t);
 
 void	intersect_cylinder(t_obj *cyl, t_intersect *inter, t_ray ray)
 {
@@ -25,6 +28,7 @@ void	intersect_cylinder(t_obj *cyl, t_intersect *inter, t_ray ray)
 
 	mult_tuple_matrix_4(&ray2.d, cyl->wtoo_m, ray.d);
 	mult_tuple_matrix_4(&ray2.o, cyl->wtoo_m, ray.o);
+	intersect_endcap(inter, cyl, &ray2);
 	q.a = ray2.d.x * ray2.d.x + ray2.d.z * ray2.d.z;
 	if (q.a < EPS)
 		return ;
@@ -50,76 +54,102 @@ static t_tuple	find_pos_inter(t_ray ray, t_u t0)
 // mais la deuxieme peux parfois etre bonne. Les deux sont a verifier.
 void	check_cylinder(t_obj *cyl, t_ray *ray2, t_intersect *inter)
 {
-	t_tuple	cyl_obj[2];
 	t_tuple i0;
 	t_tuple i1;
 
-	mult_tuple_matrix_4(&cyl_obj[0], cyl->wtoo_m, create_tuple_copy(cyl->o));
-	mult_tuple_matrix_4(&cyl_obj[1], cyl->wtoo_m, cyl->top);
 	i0 = find_pos_inter(*ray2, inter->t0_tmp);
 	i1 = find_pos_inter(*ray2, inter->t1_tmp);
-	if (i0.y > cyl_obj[0].y && i0.y < cyl_obj[1].y
-			&& i1.y > cyl_obj[0].y	&& i1.y < cyl_obj[1].y)
-		add_inter(inter, *ray2, 2, cyl);
-	else if (i0.y > cyl_obj[0].y && i0.y < cyl_obj[1].y)
-		add_inter(inter, *ray2, 0, cyl);
-	else if (i1.y > cyl_obj[0].y && i1.y < cyl_obj[1].y)
-		add_inter(inter, *ray2, 1, cyl);
-	endcap(inter, cyl, cyl_obj, ray2);
+	if (i0.y > cyl->o_obj.y && i0.y < cyl->top_obj.y
+			&& i1.y > cyl->o_obj.y	&& i1.y < cyl->top_obj.y
+			&& inter->t0 > inter->t0_tmp)
+	{
+		add_inter0(inter, cyl, inter->t0_tmp);
+		add_inter1(inter, cyl, inter->t1_tmp);
+		normal_cylinder(i0, cyl, &inter->normal_w);
+	}
+	else if (i0.y > cyl->o_obj.y && i0.y < cyl->top_obj.y
+				&& inter->t0 > inter->t0_tmp)
+	{
+		add_inter0(inter, cyl, inter->t0_tmp);
+		normal_cylinder(i0, cyl, &inter->normal_w);
+	}
+	else if (i1.y > cyl->o_obj.y && i1.y < cyl->top_obj.y
+				&& inter->t0 > inter->t1_tmp)
+	{
+		add_inter0(inter, cyl, inter->t1_tmp);
+		normal_cylinder(i1, cyl, &inter->normal_w);
+	}
 	return;
 }
 
-void	add_inter(t_intersect *inter, t_ray ray2, int index, t_obj *cyl)
-{
-	t_tuple	k;
-
-	ft_bzero(&k, sizeof(t_tuple));
-	if (index == 0)
-	{
-		inter->t0 = inter->t0_tmp;
-		k = find_pos_inter(ray2, inter->t0_tmp);
-	}
-	else if (index == 1)
-	{
-		inter->t0 = inter->t1_tmp;
-		k = find_pos_inter(ray2, inter->t1_tmp);
-	}
-	else if (index == 2)
-	{
-		inter->t0 = inter->t0_tmp;
-		inter->t1 = inter->t1_tmp;
-		k = find_pos_inter(ray2, inter->t0_tmp);
-	}
-	inter->normal_w.x = k.x;
-	inter->normal_w.y = 0;
-	inter->normal_w.z = k.z;
-	inter->normal_w.w = 0;
-	scale_v3(&inter->normal_w, -1);
-	inter->obj = cyl;
-	mult_tuple_matrix_4(&inter->normal_w, cyl->otow_m, inter->normal_w);
-}
-
-void endcap(t_intersect *inter, t_obj *cyl, t_tuple cyl_obj[2], t_ray *ray2)
+void intersect_endcap(t_intersect *inter, t_obj *cyl, t_ray *ray2)
 {
 	t_u	t0;
 	t_u	t1;
 	t_tuple i0;
 	t_tuple i1;
 
-	(void)cyl;
-	t0 = (cyl_obj[0].y - ray2->o.y) / ray2->d.y;
-	t1 = (cyl_obj[1].y - ray2->o.y) / ray2->d.y;
+	t0 = (cyl->o_obj.y - ray2->o.y) / ray2->d.y;
+	t1 = (cyl->top_obj.y - ray2->o.y) / ray2->d.y;
 	inter->t0_tmp = t0 < t1 ? t0 : t1;
 	inter->t1_tmp = t0 < t1 ? t1 : t0;
 	i0 = find_pos_inter(*ray2, inter->t0_tmp);
 	i1 = find_pos_inter(*ray2, inter->t1_tmp);
-	if (i0.x * i0.x + i0.z * i0.z <= 1)
+	if (i0.x * i0.x + i0.z * i0.z <= 1 && i1.x * i1.x + i1.z * i1.z <= 1)
 	{
-		inter->t0 = inter->t0_tmp;
+		add_inter0(inter, cyl, inter->t0_tmp);
+		add_inter1(inter, cyl, inter->t1_tmp);
+		normal_endcap(i0, cyl, &inter->normal_w);
 	}
-	if (i1.x * i1.x + i1.z * i1.z <= 1)
+	else if (i0.x * i0.x + i0.z * i0.z <= 1)
 	{
-		inter->t1 = inter->t1_tmp;
+		add_inter0(inter, cyl, inter->t0_tmp);
+		normal_endcap(i0, cyl, &inter->normal_w);
 	}
+	else if (i1.x * i1.x + i1.z * i1.z <= 1)
+	{
+		add_inter0(inter, cyl, inter->t1_tmp);
+		normal_endcap(i1, cyl, &inter->normal_w);
+	}
+}
 
+void add_inter0(t_intersect *i, t_obj *o, t_u t)
+{
+	if (t < i->t0)
+	{
+		i->t0 = t;
+		i->obj = o;
+	}
+}
+
+void add_inter1(t_intersect *i, t_obj *o, t_u t)
+{
+	if (t < i->t1)
+	{
+		i->t1 = t;
+		i->obj = o;
+	}
+}
+
+void normal_endcap(t_tuple point, t_obj *cyl, t_tuple *normal)
+{
+	normal->x = 0;
+	if (point.y >= cyl->top_obj.y - EPS)
+	normal->y = 1;
+	else if (point.y <= cyl->o_obj.y + EPS)
+	normal->y = -1;
+	normal->z = 0;
+	normal->w = 0;
+	mult_tuple_matrix_4(normal, cyl->otow_m, *normal);
+	scale_v3(normal, -1);
+}
+
+void normal_cylinder(t_tuple point, t_obj *cyl, t_tuple *normal)
+{
+	normal->x = point.x;
+	normal->y = 0;
+	normal->z = point.z;
+	normal->w = 0;
+	mult_tuple_matrix_4(normal, cyl->otow_m, *normal);
+	scale_v3(normal, -1);
 }
